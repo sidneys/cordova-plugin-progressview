50fcd472d282b35ed879425016dad569e806d763
Sidney Bofah <sidney.bofah@googlemail.com>
2014-11-23 00:30:47 +0100

    Add rest of MRProgress framework files. Reference in plugin.xml still missing (no auto-install)

---
+++ src/ios/MRProgress/MRProgress.h
@@ -0,0 +1,17 @@
+//
+//  MRProgress.h
+//  MRProgress
+//
+//  Created by Marius Rackwitz on 20.10.13.
+//  Copyright (c) 2013 Marius Rackwitz. All rights reserved.
+//
+
+#import "MRActivityIndicatorView.h"
+#import "MRBlurView.h"
+#import "MRCircularProgressView.h"
+#import "MRIconView.h"
+#import "MRMessageInterceptor.h"
+#import "MRNavigationBarProgressView.h"
+#import "MRProgressOverlayView.h"
+#import "MRWeakProxy.h"
+#import "UIImage+MRImageEffects.h"
